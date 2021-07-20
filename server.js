c942e41decdf012ffe63553558f8ab0c0cca69c5
Erin Blaize <eeblaize2@hotmail.com>
2021-07-20 11:27:44 -0700

    added variable

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ app.post('/search', (req, res)=>{
  
 
 if(process.env.NODE_ENV==='production'){
+  const __dirname = path.resolve()
   app.use(express.static(path.join(__dirname, '/client/build')))
   app.get('*', (req, res)=>{
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
